a9befa101400827dd4af8791f93653a144400d5e
sarat532 <sarat.kota@gmail.com>
2021-05-26 21:19:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          But this is going to be synced.
         </p>
         <a
           className="App-link"
